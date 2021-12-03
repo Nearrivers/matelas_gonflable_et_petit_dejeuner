df4c2de348ce7479ed24ac8bdc120a014d1e9d67
Adam Odienne <odienne.adam@gmail.com>
2021-12-03 16:12:16 +0100

    adding boostrap and webpack config

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   resources :categories
   resources :l_options
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'locations#index'
 end
--- db/migrate/20211203133910_add_attribute_to_message.rb
+++
@@ -1,5 +0,0 @@
-class AddAttributeToMessage < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :messages, :conversation, null: true, foreign_key: true
-  end
-end
