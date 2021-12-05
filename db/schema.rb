94ebf21e821a89ade4668d732c3f759a36d0047d
Fourcade Antoine <a.fourcade65@gmail.com>
2021-12-05 11:32:40 +0100

    Navbar style done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_131743) do
+ActiveRecord::Schema.define(version: 2021_12_03_160525) do
 
   create_table "categories", force: :cascade do |t|
     t.string "value"
@@ -106,6 +106,16 @@ ActiveRecord::Schema.define(version: 2021_12_03_131743) do
     t.string "stripe"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "equipment", "categories"
