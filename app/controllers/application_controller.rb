75dd4cf8bee1080b978e69c9a89728bdd9c1e692
Adam Odienne <odienne.adam@gmail.com>
2022-02-03 15:39:31 +0100

    querying latest locations and best locations for index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
 
   def index
-    @latest_locations = Location.limit(4)
-    @latest_locations.inspect
+    @great_locations = Location.order('avg_score DESC').limit(4)
+    @latest_locations = Location.order('id DESC').limit(4)
 
     render template: '/layouts/_latest_locations'
   end
