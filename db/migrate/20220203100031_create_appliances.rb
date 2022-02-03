a6e1ecf0cd3d2adf6d1a4961ff49ecebd9987a48
Fourcade Antoine <a.fourcade65@gmail.com>
2022-02-03 14:32:03 +0100

    feat(appliances): displaying location's appliance on single location

--- db/migrate/20220203100031_create_appliances.rb
+++ db/migrate/20220203100031_create_appliances.rb
@@ -1,5 +1,6 @@
 class CreateAppliances < ActiveRecord::Migration[6.1]
   def change
+    drop_table :appliances
     create_table :appliances do |t|
       t.string :value
       t.references :category, null: false, foreign_key: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,28 +13,12 @@ categories_list = [
 ]
 
 appliances_list = [
-    ["Piscine", 1],
-    ["Jacuzzi", 1],
-    ["Patio", 1],
-    ["Barbecue", 1],
-    ["Brasero", 1],
-    ["Billard", 1],
-    ["Cheminée", 1],
-    ["Espace repas en plein air", 1],
-    ["Appareils de fitness", 1],
     ["Wifi", 2],
+    ["Ascenseur", 2],
     ["Télévision", 2],
     ["Cuisine", 2],
     ["Lave-linge", 2],
-    ["Parking gratuit sur place", 2],
-    ["Parking payant sur place", 2],
     ["Climatisation", 2],
-    ["Espace de travail dédié", 2],
-    ["Douche extérieure", 2],
-    ["Détecteur de fumée", 3],
-    ["Kit de premiers secours", 3],
-    ["Détecteur de monoxyde de carbone", 3],
-    ["Porte de la chambre avec verrou", 3],
     ["Extincteur", 3]
 ]
 
